441e8fbf8e58b382b03b74f27a6c2057459f738e
Chris Lamb <chris@chris-lamb.co.uk>
2014-07-14 15:47:02 +0100

    A few touch-ups.
    Signed-off-by: Chris Lamb <chris@chris-lamb.co.uk>

--- bananas/utils/models.py
+++ bananas/utils/models.py
@@ -1,9 +1,7 @@
 from django import template
 
 from . import youtube_code_block
-from . import pygments_code_block
 
-template.add_to_builtins('bananas.utils.templatetags.bootstrap')
 template.add_to_builtins('bananas.utils.templatetags.fonts')
 template.add_to_builtins('bananas.utils.templatetags.media')
 template.add_to_builtins('bananas.utils.templatetags.python')
@@ -12,4 +10,3 @@ template.add_to_builtins('cache_toolbox.templatetags.cache_toolbox')
 template.add_to_builtins('django_autologin.templatetags.django_autologin')
 
 youtube_code_block = youtube_code_block # appease linters
-pygments_code_block = pygments_code_block # appease linters

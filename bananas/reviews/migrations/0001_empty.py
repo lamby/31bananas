3e0ca2b5118dd182158362ca3d0b6dc0d3110879
Chris Lamb <chris@chris-lamb.co.uk>
2014-07-15 19:41:20 +0100

    Empty migration.
    Signed-off-by: Chris Lamb <chris@chris-lamb.co.uk>

---
+++ bananas/reviews/migrations/0001_empty.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        pass
+
+    def backwards(self, orm):
+        pass
+
+    models = {
+        u'reviews.review': {
+            'Meta': {'ordering': "('-date',)", 'object_name': 'Review'},
+            'content': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'created': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.utcnow'}),
+            'date': ('django.db.models.fields.DateField', [], {'unique': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'published': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '250', 'blank': 'True'})
+        }
+    }
+
+    complete_apps = ['reviews']

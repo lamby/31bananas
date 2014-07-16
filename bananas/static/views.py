65489010970bdc3a9fd9b19dbf158d11ea1802b1
Chris Lamb <chris@chris-lamb.co.uk>
2014-07-16 20:24:32 +0100

    A bit more.s

--- bananas/static/views.py
+++ bananas/static/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
 
+from bananas.reviews.models import Review
+
 def landing(request):
-    return render(request, 'static/landing.html')
+    return render(request, 'static/landing.html', {
+        'reviews': Review.objects.public(),
+    })

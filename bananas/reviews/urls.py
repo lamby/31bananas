260c5ccbfe241208136f210a4f04a3e09fa710a6
Chris Lamb <chris@chris-lamb.co.uk>
2014-07-16 22:47:17 +0100

    Nicer url
    Signed-off-by: Chris Lamb <chris@chris-lamb.co.uk>

--- bananas/reviews/urls.py
+++ bananas/reviews/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import patterns, url, include
 urlpatterns = patterns('bananas.reviews.views',
     url(r'', include('bananas.reviews.reviews_admin.urls', namespace='admin')),
 
-    url(r'^(?P<day>\d+)-(?P<slug>[^\/]*)$', 'view',
+    url(r'^day-(?P<day>\d+)-(?P<slug>[^\/]*)$', 'view',
         name='view'),
 )

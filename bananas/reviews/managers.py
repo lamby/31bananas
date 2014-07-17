997bc1c2d400078ff074d2c3a80966de8912491d
Chris Lamb <chris@chris-lamb.co.uk>
2014-07-17 10:11:52 +0100

    Auto publish!
    Signed-off-by: Chris Lamb <chris@chris-lamb.co.uk>

--- bananas/reviews/managers.py
+++ bananas/reviews/managers.py
@@ -2,8 +2,14 @@ import datetime
 
 from django.db import models
 
+# 10AM BST
+PUBLISH_AT = datetime.time(9, 15)
+
 class ReviewManager(models.Manager):
     def public(self):
-        return self.filter(
-            date__lte=datetime.datetime.today(),
-        )
+        epoch = datetime.datetime.utcnow()
+
+        if epoch.time() < PUBLISH_AT:
+            epoch -= datetime.timedelta(days=1)
+
+        return self.filter(date__lte=epoch.date())

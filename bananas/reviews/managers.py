8e8126b9a7d2b4a10b6b2dbee47bd44640683b13
Chris Lamb <chris@chris-lamb.co.uk>
2014-07-17 07:59:32 +0100

    published.
    Signed-off-by: Chris Lamb <chris@chris-lamb.co.uk>

--- bananas/reviews/managers.py
+++ bananas/reviews/managers.py
@@ -4,4 +4,7 @@ from django.db import models
 
 class ReviewManager(models.Manager):
     def public(self):
-        return self.filter(date__lte=datetime.datetime.today())
+        return self.filter(
+            date__lte=datetime.datetime.today(),
+            published=True,
+        )

a9b573cd070dd0024bc5477bfb57605093104197
Chris Lamb <chris@chris-lamb.co.uk>
2014-07-19 12:29:43 +0100

    Weird suffix.

--- bananas/reviews/reviews_admin/reviews_admin_images/urls.py
+++ bananas/reviews/reviews_admin/reviews_admin_images/urls.py
@@ -5,7 +5,7 @@ urlpatterns = patterns('bananas.reviews.reviews_admin.reviews_admin_images.views
         name='xhr-view'),
     url(r'^xhr/admin/(?P<review_id>\d+)/images/add$', 'add',
         name='add'),
-    url(r'^xhr/admin/(?P<review_id>\d+)/images/add/url-u$', 'xhr_add_url',
+    url(r'^xhr/admin/(?P<review_id>\d+)/images/add/url$', 'xhr_add_url',
         name='xhr-add-url'),
     url(r'^xhr/admin/(?P<review_id>\d+)/images/(?P<image_id>\d+)$', 'xhr_delete',
         name='xhr-delete'),

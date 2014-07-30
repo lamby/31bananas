9b1b76a722a8b7252ca7d414de175f24a7bfee25
Chris Lamb <chris@chris-lamb.co.uk>
2014-07-30 20:44:10 +0100

    Publish when I say.
    Signed-off-by: Chris Lamb <chris@chris-lamb.co.uk>

--- bananas/reviews/managers.py
+++ bananas/reviews/managers.py
@@ -1,18 +1,5 @@
-import datetime
-
 from django.db import models
-
-# 10AM BST
-PUBLISH_AT = datetime.time(9, 15)
 
 class ReviewManager(models.Manager):
     def public(self):
-        epoch = datetime.datetime.utcnow()
-
-        if epoch.time() < PUBLISH_AT:
-            epoch -= datetime.timedelta(days=1)
-
-        return self.filter(
-            date__lte=epoch.date(),
-            published=True,
-        )
+        return self.filter(published=True)

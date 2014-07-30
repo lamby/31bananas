b4ec7f56ae3bf95aa7ede9b26ec8917a1d676aeb
Chris Lamb <chris@chris-lamb.co.uk>
2014-07-30 20:49:27 +0100

    Expose range()
    Signed-off-by: Chris Lamb <chris@chris-lamb.co.uk>

--- bananas/utils/templatetags/python.py
+++ bananas/utils/templatetags/python.py
@@ -2,6 +2,10 @@ from django import template
 
 register = template.Library()
 
+@register.filter('range')
+def range_(val):
+    return range(val)
+
 @register.filter
 def getitem(obj, att):
     if obj:

d36addafe9bfbeb917652411f1b5ffb6372144ab
Chris Lamb <chris@chris-lamb.co.uk>
2014-07-31 17:26:43 +0100

    reorder.

--- bananas/urls.py
+++ bananas/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, url, include
 
 urlpatterns = patterns('',
-    url(r'', include('bananas.reviews.urls', namespace='reviews')),
     url(r'', include('bananas.debug.urls', namespace='debug')),
+    url(r'', include('bananas.reviews.urls', namespace='reviews')),
     url(r'', include('bananas.static.urls', namespace='static')),
 )

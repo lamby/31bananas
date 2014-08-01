e8745308b6b8107131379a101b87a77b42af0f49
Chris Lamb <chris@chris-lamb.co.uk>
2014-08-01 08:25:03 +0100

    ah, querystring auth
    Signed-off-by: Chris Lamb <chris@chris-lamb.co.uk>

--- bananas/settings/defaults/__init__.py
+++ bananas/settings/defaults/__init__.py
@@ -126,7 +126,7 @@ SITE_URL = 'http://31bananas.co.uk'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 
 AWS_ACCESS_KEY_ID = 'AKIAJ2MH6KAU3WOSKPCQ'
-AWS_S3_SECURE_URLS = False
+AWS_QUERYSTRING_AUTH = False
 AWS_SECRET_ACCESS_KEY = 'PFc5/5lERCkf3uNj+Icyx0PHe9ZrHcdemw/Y1kqr'
 AWS_STORAGE_BUCKET_NAME = 'lamby-bananas'
 
